f14be776900cb90e631eb84278709c348664f432
MaxKroshka <max.kroshka@gmail.com>
2016-01-13 14:41:57 -0800

    Fix someInstance and make progress towards pop and push.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
 var Stack = function() {
   var someInstance = {};
+  var stackLength = 0; 
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    stackLength++; 
+    storage[stackLength] = value; 
   };
 
   someInstance.pop = function() {
+    if(stackLength > 0){
+      stackLength--;
+    }
   };
 
   someInstance.size = function() {
+    return stackLength; 
   };
 
   return someInstance;
