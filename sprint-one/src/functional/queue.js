782aa7914478c1f87c4f52dafac9a584bd4b051e
MaxKroshka <max.kroshka@gmail.com>
2016-01-13 14:52:19 -0800

    Fix size method; dequeue and queue incrementing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,21 @@
 var Queue = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
+  var stackSize = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    stackSize++;
   };
 
   someInstance.dequeue = function() {
+    stackSize--;
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
