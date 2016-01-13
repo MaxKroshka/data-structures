1b8ae338f35f2428b4f226cc4489f64ad32b68d9
MaxKroshka <max.kroshka@gmail.com>
2016-01-13 15:59:49 -0800

    Include extend method and return correct size

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {}; 
+  var stackSize = 0;
+
+  newStack.size = function(){
+    return stackSize;
+  };
+  newStack.push = function(){
+    stackSize++; 
+  };
+
+  _.extend(newStack,Stack.stackMethods); 
+  return newStack;
 };
 
-var stackMethods = {};
 
+var stackMethods = {};
 
