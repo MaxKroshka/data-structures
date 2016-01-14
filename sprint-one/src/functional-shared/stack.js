9c4e09150bbd6a33518b1b71a697ac321f83a840
Max Kroshka <max.kroshka@gmail.com>
2016-01-13 22:14:42 -0800

    Remove commented-out code to improve readability of the constructor function

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,53 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var newStack = {}; 
+  var newStack = {};
   newStack.stackSize = 0;
   newStack.storage = {};
-
-  // newStack.size = function(){
-  //   return stackSize;
-  // };
-
-  // newStack.push = function(value){
-  //   stackSize++;
-  //   storage[stackSize] = value;
-  // };
-
-  // newStack.pop = function(){
-  //   if(stackSize > 0){
-  //     var temp = storage[stackSize];
-  //     delete storage[stackSize];
-  //     stackSize--;
-  //     return temp;
-  //   }
-  // };
-
-  _.extend(newStack,stackMethods); 
-
+  _.extend(newStack, stackMethods);
   return newStack;
 
 };
 
-
 var stackMethods = {
 
-  size : function(){
+  size: function() {
     return this.stackSize;
   },
 
-  push : function(value){ 
+  push: function(value) {
     this.stackSize++;
     this.storage[this.stackSize] = value;
   },
 
-  pop : function(){
-    if(this.stackSize > 0){
+  pop: function() {
+    if (this.stackSize > 0) {
       var temp = this.storage[this.stackSize];
       delete this.storage[this.stackSize];
       this.stackSize--;
       return temp;
     }
   }
-  
+
 };
