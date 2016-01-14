66d58f22cce45c76efb5754bf5e879c4baf68f20
MaxKroshka <max.kroshka@gmail.com>
2016-01-14 11:15:24 -0800

    Pass all the queue length related manipulations

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.queueLength = 0;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function(){
+    return this.queueLength;
+  },
+  enqueue: function(value){
+    this.queueLength++;
+  },
+  dequeue: function(){
+    if(this.queueLength > 0){
+      this.queueLength--;
+    }
+  }
+};
 
 
