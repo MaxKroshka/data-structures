d556d26c7bdb1fb86f11caba5922d166bbb903d9
MaxKroshka <max.kroshka@gmail.com>
2016-01-14 15:56:42 -0800

    Complete all tests for the stack pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,6 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.stackSize = 0; 
+  this.store = {}; 
 };
 
 Stack.prototype.size = function(){
@@ -9,10 +10,15 @@ Stack.prototype.size = function(){
 }; 
 
 Stack.prototype.push = function(value){
-  t
   this.stackSize++; 
+  this.store[this.stackSize] = value; 
 }; 
 
 Stack.prototype.pop = function(){
-  this.stackSize --; 
-}
+  if(this.stackSize>0){
+    var temp = this.store[this.stackSize]; 
+    delete this.store[this.stackSize]; 
+    this.stackSize --; 
+    return temp; 
+  }
+}; 
