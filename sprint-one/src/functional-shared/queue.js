510ce21a9dbe5119522bc05f878625e5783f06ad
MaxKroshka <max.kroshka@gmail.com>
2016-01-14 10:16:18 -0800

    Set-up environment for queue and pass size method test.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.stackSize = 0; 
+  instance.storage = {};
+  instance.uniqueKey = 0; 
+
+  _.extend(instance, Queue.queueMethods);
+
+  return instance; 
+
 };
 
-var queueMethods = {};
+Queue.queueMethods = {
+  size: function(){
+    return this.stackSize;
+  } 
+
+};
 
 
