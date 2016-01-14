25b68a348917acd4d052390707d2d47c4470f17c
MaxKroshka <max.kroshka@gmail.com>
2016-01-14 10:59:36 -0800

    Fix all stack size related manipulations

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackObject = Object.create(stackMethods);
+  stackObject.stackSize = 0;
+  return stackObject;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function(){
+    return this.stackSize;
+  },
+  push: function(value){
+    this.stackSize++;
+    // this.stackObject[stackSize] = value; 
+  },
+  pop: function(){
+    if(this.stackSize > 0){
+    this.stackSize--;
+    }
+  }
+};
 
+
+
+  // Stack.prototype.size =  function(){
+  //   return this.stackSize;
+  // };
+  // Stack.prototype.push = function(value){
+  //   this.stackSize++;
+  //   this.stackObject[stackSize] = value; 
+  // };
+  // Stack.prototype.pop = function(){
+  //   if(this.stackSize > 0){
+  //   this.stackSize--;
+  //   }
+  // };
 
