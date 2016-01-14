cc9634ce44078599d7625149bb65d9bccc8f255a
MaxKroshka <max.kroshka@gmail.com>
2016-01-14 15:59:34 -0800

    Implement stacksize, queue, and dequque for the queue pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {}; 
+  this.stackSize = 0; 
+
 };
+
+
+Queue.prototype.size = function(){
+  return this.stackSize; 
+};
+
+Queue.prototype.enqueue = function(value){
+  this.stackSize ++;
+};
+
+Queue.prototype.dequeue = function(){
+  this.stackSize --; 
+}
 
 
