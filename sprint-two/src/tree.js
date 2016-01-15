5545c4e700342f0252b5c862b2dec9d11b9f348f
MaxKroshka <max.kroshka@gmail.com>
2016-01-14 20:12:48 -0800

    implent contain method that works on the parent tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,7 +15,17 @@ treeMethods.addChild = function(value) {
   this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, tree) {
+  tree = tree || this;
+  for(var i=0;i<tree.children.length;i++){
+    if(tree.children[i].value === target){
+      return true; 
+    } else if (tree.children[i].children.length > 0) {
+      return tree.contains(target, tree.children[i]);
+    } 
+  }
+  return false; 
+
 };
 
 
