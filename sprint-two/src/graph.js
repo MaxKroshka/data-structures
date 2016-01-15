f5a9eb438c0f72c08889c570e89877a3d5cddab5
MaxKroshka <max.kroshka@gmail.com>
2016-01-15 11:36:34 -0800

    Implement addNode, addEdge, contains and hasEdge methods for Graph data structure

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,33 +1,41 @@
-
+ 
 
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.graphData = {};
+
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.graphData[node] = {connections : []}; 
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+ return this.graphData[node] !== undefined;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.graphData[node];
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return (this.graphData[fromNode].connections.indexOf(toNode) > -1 || this.graphData[toNode].connections.indexOf(fromNode) > -1);
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.graphData[fromNode].connections.push(toNode);
+  this.graphData[toNode].connections.push(fromNode);
 };
 
 // ------------------------
