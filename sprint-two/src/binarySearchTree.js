a75591b776dd467a20adf20a06fba850ab27ae0c
MaxKroshka <max.kroshka@gmail.com>
2016-01-15 20:27:58 -0800

    Implement basic functionality of BinaryTree data structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,48 @@
 var BinarySearchTree = function(value) {
+  var newTree = {}; 
+  newTree.value = value; 
+
+  newTree.insert = function(value){
+    var currentVal = this.value; 
+    if(currentVal > value){
+      if(newTree.left === undefined){
+        newTree.left = BinarySearchTree(value);   
+      } else {
+        if(newTree.left.value > value){
+          newTree.left.left = BinarySearchTree(value);
+        }
+        else {
+          newTree.left.right = BinarySearchTree(value); 
+        }
+      }
+    } else {
+      if(newTree.left === undefined){
+        newTree.right = BinarySearchTree(value);   
+      } else {
+        if(newTree.right.value > value){
+          newTree.right.left = BinarySearchTree(value);
+        }
+        else {
+          newTree.right.right = BinarySearchTree(value); 
+        }
+      }
+       
+    }
+  };
+
+  newTree.contains = function(){
+  };
+
+  newTree.depthFirstLog = function(){
+  };
+
+
+  return newTree; 
 };
+
+
+
+
 
 
 /*
